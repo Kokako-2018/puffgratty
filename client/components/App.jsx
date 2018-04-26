7726b6022be5afcff3b15e915864bc413259837d
maddyking44 <madelineking44@gmail.com>
2018-04-26 22:09:32 +1200

    setup

---
+++ client/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class App extends React.Component {
+    render(){
+        return (
+            <p>i fell like we should make this non stateful</p>
+        )
+    }
+}
+export default App
