333188a590c6c3b2b8ff36261ab142e423be40e3
Rosie Burkett <burkettroz@gmail.com>
2018-04-27 10:24:42 +1200

    Created Components

---
+++ components/Main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Puff from './Puff'
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    } 
+  }
+
+
+  render(){
+    return(
+      <Puff/>
+    )
+  }
+}
+
+export default Main
---
+++ components/Puff.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class Puff extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    } 
+  }
+
+
+  render(){
+    return(
+      <h1>tag two</h1>
+    )
+  }
+}
+
+export default Puff
