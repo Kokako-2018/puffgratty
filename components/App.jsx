30a55664eac21d206fa39a25007eab9970bece5c
anugrah-danang-abiyoga <danangabiyoga26@gmail.com>
2018-04-27 15:49:25 +1200

    animation

--- components/App.jsx
+++ components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Main from './Main'
 import {HashRouter as Router, Route, Link} from 'react-router-dom'
 import ItemsContainer from '../containers/ItemsContainer.js'
-
+import Animation from './Cloud/Animation'
 
 class App extends React.Component {
   constructor(props) {
@@ -19,7 +19,8 @@ class App extends React.Component {
       <Router>
         <React.Fragment>
           <Route exact path='/form' component={ItemsContainer}/>
-          <Route exact path='/' component={Main}/>          
+          <Route exact path='/' component={Main}/>
+          <Route exact path='/animation' component={Animation} />          
         </React.Fragment>
       </Router>
     )
